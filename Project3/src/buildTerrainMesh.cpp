c335a8fc94da8016253d06c0a0088cc0ff5392f1
ShockDexterity <90293186+ShockDexterity@users.noreply.github.com>
2023-03-02 13:20:54 -0600

    added buildTerrainMesh.cpp

---
+++ Project3/src/buildTerrainMesh.cpp
@@ -0,0 +1,6 @@
+#include "buildTerrainMesh.h"
+
+void buildTerrainMesh(ofMesh& terrainMesh, const ofShortPixels& heightmap, unsigned int xStart, unsigned int yStart, unsigned int xEnd, unsigned int yEnd, glm::vec3 scale)
+{ 
+	return;
+}
