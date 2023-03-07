2c22de48ca12f3ae80b5b4655792ba9ddbd0c97b
ShockDexterity <90293186+ShockDexterity@users.noreply.github.com>
2023-03-07 11:41:31 -0600

    Update buildTerrainMesh.cpp

--- Project3/src/buildTerrainMesh.cpp
+++ Project3/src/buildTerrainMesh.cpp
@@ -23,14 +23,12 @@ void buildTerrainMesh(ofMesh& terrainMesh, const ofShortPixels& heightmap, unsig
 	{
 		for (unsigned int x { xStart }; x < xEnd - 1; ++x)
 		{
-			std::vector<unsigned int> vec {};
-
 			const unsigned int i1 { x + y * xEnd };
-			const unsigned int i2 { x + (y + 1) * xEnd+1 };
+			const unsigned int i2 { x + (y + 1) * xEnd + 1 };
 			const unsigned int i3 { (x + 1) + y * xEnd };
 			const unsigned int i4 { (x + 1) + y * xEnd };
-			const unsigned int i5 { x + (y + 1) * xEnd +1};
-			const unsigned int i6 { (x + 1) + (y + 1) * xEnd +1};
+			const unsigned int i5 { x + (y + 1) * xEnd + 1 };
+			const unsigned int i6 { (x + 1) + (y + 1) * xEnd + 1 };
 
 			terrainMesh.addIndex(i1);
 			terrainMesh.addIndex(i2);
@@ -39,12 +37,7 @@ void buildTerrainMesh(ofMesh& terrainMesh, const ofShortPixels& heightmap, unsig
 			terrainMesh.addIndex(i5);
 			terrainMesh.addIndex(i6);
 
-			vec.emplace_back(i1);
-			vec.emplace_back(i2);
-			vec.emplace_back(i3);
-			vec.emplace_back(i4);
-			vec.emplace_back(i5);
-			vec.emplace_back(i6);
+			std::vector<unsigned int> vec { i1,i2,i3,i4,i5,i6 };
 
 			if (!done)
 			{
